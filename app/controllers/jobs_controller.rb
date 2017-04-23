2625d9321879491f7c9055616bfb0d75d59294e1
silvia <gitz1594@icloud.com>
2017-04-23 17:07:39 +0800

    fix bug

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,5 @@
 class JobsController < ApplicationController
  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
- before_action :require_is_admin
 
  def index
   @jobs = case params[:order]
