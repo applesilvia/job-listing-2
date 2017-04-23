c07d0f26149bf0740a7b8b2bab98b452badb2037
silvia <gitz1594@icloud.com>
2017-04-23 12:53:15 +0800

    step 6 order by wage

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,14 +3,14 @@ class JobsController < ApplicationController
  before_action :require_is_admin
 
     def index
-        @jobs = case params[:order]
-            when 'by_lower_bound'
-            Job.published.order('wage_lower_bound DESC')
-            when 'by_upper_bound'
-            Job.published.order('wage_upper_bound DESC')
-            else
-            Job.published.recent
-            end
+      @jobs = case params[:order]
+          when 'by_lower_bound'
+          Job.published.order('wage_lower_bound DESC')
+          when 'by_upper_bound'
+          Job.published.order('wage_upper_bound DESC')
+          else
+          Job.published.recent
+          end
     end
 
   def show
